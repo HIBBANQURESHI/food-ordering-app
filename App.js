eb19ddd0ce5860f4437d7ce2bd5b7970d9c52a48
HIBBANQURESHI <qureshihibban@gmail.com>
2025-03-06 01:41:27 +0500

    added

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DetailsScreen from './screens/DetailsScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator ( ) ;
 
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Home',
-  screenOptions: {
-    headerStyle: { backgroundColor: 'tomato' },
-  },
-  screens: {
-    Home: {
-      screen: HomeScreen,
-      options: {
-        title: 'Overview',
-      },
-    },
-    DetailsScreen: DetailsScreen,
-  },
-});
-const Navigation = createStaticNavigation(RootStack);
-
-export default function App() {
-  return <Navigation />;
+export default function App ( ) {
+  return(
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+)
 }
